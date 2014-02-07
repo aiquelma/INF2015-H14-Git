0adbb41896d71fa4ee3a56450ae793f3c63371c5
Marc-Andre Aiquel <aiquel@me.com>
2014-02-06 21:07:34 -0500

    initialization du projet
    deuxieme ligne print

--- src/projetgit/ProjetGit.java
+++ src/projetgit/ProjetGit.java
@@ -17,7 +17,7 @@ public class ProjetGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello Git");
+        System.out.println("Hello GitHub");
     }
     
 }
